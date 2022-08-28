9400b7316ddcfcf4a46116dfee905590fd2fe65e
AhmadRaza <ahmadraza.azt@gmail.com>
2022-08-28 22:25:14 +0500

    done login siginup

--- app.js
+++ app.js
@@ -2,8 +2,8 @@
 
     var mail = ("example@gmail.com");
     var pass = ("12346");
-    localStorage.setItem("email", mail);  
-    localStorage.setItem("password", pass);  
+    localStorage.setItem("email1", mail);  
+    localStorage.setItem("password1", pass);  
 
 function signUp(){
     var fullName = document.getElementById("fullName").value;
@@ -14,14 +14,47 @@ function signUp(){
     localStorage.setItem("fullName", fullName);  
     localStorage.setItem("email", email);  
     localStorage.setItem("password", password);  
-    localStorage.setItem("password", rePassword); 
+    localStorage.setItem("repassword", rePassword); 
     
     
 }
-    
-    
+function login(){
+    var loginEmail = document.getElementById("email").value;
+    var loginPassword = document.getElementById("password").value;
+    var email = localStorage.getItem("email");
+    var password = localStorage.getItem("password");
 
 
 
+
+    // var loginEmail = "";
+    // var loginPassword = "";
+
+
+
+    if (loginEmail === email && loginPassword === password || loginEmail === email && loginPassword === password ) {
+        alert("succesfull");
+
+        // var div = document.createElement('div');
+        //     div.className = "alert alert-success";
+        //     div.appendChild("body")
+        //     document.createTextNode("A simple success alert—check it out!");
+   
+    }   else {
+        alert("Invalid Email or Password");
+        // var div = document.createElement('div');
+        // div.className = "alert alert-danger";
+        // div.appendChild("body");
+        // document.createTextNode("Invalid Email Or Password")
+        
+    }
+
+}
+
+        // var main = document.getElementById('main');
+        // var div = document.createElement('div');
+        //     div.className = "alert alert-success";
+        //     main.appendChild("div")
+        //     document.createTextNode("A simple success alert—check it out!");
 // var latitude = localStorage.getItem("latitude");
 // localStorage.setItem("longitude", response.data.location.lon);  
